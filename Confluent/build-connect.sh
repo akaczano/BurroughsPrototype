d4a5aed77767b4d84c3031204f5335779623e0be
Aidan Kaczanowski <akaczano@purdue.edu>
2021-02-05 12:24:13 -0500

    Cleaned things up and adjusted documentation

--- Confluent/build-connect.sh
+++
@@ -1,5 +0,0 @@
-cd ../SingleMessageTransforms
-mvn clean package
-cd ../Confluent
-cp ../SingleMessageTransforms/target/BurroughsSMT-1.0-SNAPSHOT.jar ./BurroughsSMT.jar
-docker image build -t customconnect:latest .
