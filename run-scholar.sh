aa8923613347dae2f688b76db59dbc9f6d3203a6
Aidan Kaczanowski <akaczano@purdue.edu>
2021-02-05 15:52:11 -0500

    Added script for running Burroughs against Confluent Platform on Scholar

---
+++ run-scholar.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+docker run --rm -it \
+-v $(pwd)/producer:/producer \
+--network host \
+-e KSQL_HOST=http://172.18.30.60:32428 \
+-e DB_HOST=172.18.30.60:30123 \
+-e DB_PASSWORD=password \
+-e KAFKA_HOST=172.18.30.60:32275 \
+-e SCHEMA_REGISTRY=http://172.18.30.60:32388 \
+burroughs
